467d05cd2869fcb5b1387962c4e2752ee053807e
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-02-23 19:24:47 +0300

    started work with database module

--- db/init.go
+++ db/init.go
@@ -1,1 +1,39 @@
 package db
+
+import (
+	"github.com/jackc/pgx"
+	"log"
+	"sync"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "park"
+	password = "admin"
+	dbname   = "park_forum"
+)
+
+var db *pgx.ConnPool = nil
+var syncOnce = sync.Once{}
+
+func ConnectToDB() *pgx.ConnPool {
+	syncOnce.Do(func() {
+		pgxConfig := pgx.ConnConfig{
+			Host:     host,
+			Port:     port,
+			Database: dbname,
+			User:     user,
+			Password: password,
+		}
+		pgxConnPoolConfig := pgx.ConnPoolConfig{
+			ConnConfig: pgxConfig,
+		}
+		dbase, err := pgx.NewConnPool(pgxConnPoolConfig)
+		if err != nil {
+			log.Fatal("Connection to database was failed")
+		}
+		db = dbase
+	})
+	return db
+}
--- db/models.go
+++ db/models.go
@@ -1,1 +1,32 @@
 package db
+
+import "time"
+
+type User struct {
+	Uid      int    `json:"uid"`
+	Phone    string `json:"phone"`
+	Email    string `json:"email"`
+	Password []byte `json:"-"`
+}
+
+const (
+	male = iota
+	female
+	other
+)
+
+type LocationPoint struct {
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"lng"`
+	Accuracy  int     `json:"accuracy"`
+}
+
+type UserInfo struct {
+	About     string        `json:"about"`
+	Photos    []string      `json:"photos"`
+	Rating    float32       `json:"rating"`
+	Birthday  time.Time     `json:"birthday"`
+	Gender    int           `json:"gender"`
+	LoginDate time.Time     `json:"login_date"`
+	Location  LocationPoint `json:"location"`
+}
