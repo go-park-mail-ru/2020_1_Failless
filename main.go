89e9ca1334464f8ca8982a671ba34220776c7945
Failless God <failless@rowbot.dev>
2020-02-28 12:06:51 +0000

    added ssl

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 		WriteTimeout: time.Second * 30,
 	}
 	log.Println("server is running on " + strconv.Itoa(serverSettings.Port))
-	err := server.ListenAndServeTLS("/etc/ssl/certs/ssl-bundle/bundle.crt", "/etc/ssl/certs/ssl-bundle/private.key.pem")
+	err := server.ListenAndServeTLS("./configs/ssl-bundle/bundle.crt", "./configs/ssl-bundle/private.key.pem")
 	if err != nil {
 		fmt.Println(err)
 	}
