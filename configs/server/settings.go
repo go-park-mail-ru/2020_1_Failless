538cd0623c0a9f5c3d7460408b1d359359ab80fc
Failless God <failless@rowbot.dev>
2020-03-22 10:17:15 +0000

    moved from ssl on backend to the nginx proxy

--- configs/server/settings.go
+++ configs/server/settings.go
@@ -106,10 +106,10 @@ func GetConfig() *settings.ServerSettings {
 			AllowedHosts: map[string]struct{}{
 				"http://localhost":           {},
 				"http://localhost:8080":      {},
-				"http://localhost:5000":      {},
+				"http://localhost:3000":      {},
 				"http://127.0.0.1":           {},
 				"http://127.0.0.1:8080":      {},
-				"http://127.0.0.1:5000":      {},
+				"http://127.0.0.1:3000":      {},
 				"https://eventum.rowbot.dev": {},
 			},
 		}
