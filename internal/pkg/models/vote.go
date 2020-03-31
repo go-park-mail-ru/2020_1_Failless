01d1821caa0c3e064d5642f7c561239360f61f84
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-03-31 15:30:21 +0300

    added like|dislike insertions to the database

--- internal/pkg/models/vote.go
+++ internal/pkg/models/vote.go
@@ -9,3 +9,11 @@ type Vote struct {
 	Date  time.Time `json:"-"`
 }
 
+type Chat struct {
+	ChatId    int       `json:"chat_id"`
+	AdminId   int       `json:"admin_id"`
+	Eid       int       `json:"eid"`
+	Created   time.Time `json:"created, omitempty"`
+	UserCount int       `json:"user_count, omitempty"`
+	Title     string    `json:"title, omitempty"`
+}
--- internal/pkg/vote/usecase/model.go
+++ internal/pkg/vote/usecase/model.go
@@ -6,6 +6,7 @@ import (
 	"failless/internal/pkg/network"
 	"failless/internal/pkg/vote"
 	"failless/internal/pkg/vote/repository"
+	"net/http"
 )
 
 type voteUseCase struct {
@@ -19,5 +20,30 @@ func GetUseCase() vote.UseCase {
 }
 
 func (vc *voteUseCase) VoteEvent(vote models.Vote) network.Message {
-	return network.Message{}
+	vote.Value = vc.ValidateValue(vote.Value)
+	err := vc.rep.AddEventVote(vote.Uid, vote.Id, vote.Value)
+	// i think that there could be an error in one case - invalid event id
+	if err != nil {
+		return network.Message{
+			Request: nil,
+			Message: err.Error(),
+			Status:  http.StatusBadRequest,
+		}
+	}
+
+	return network.Message{
+		Request: nil,
+		Message: "OK",
+		Status:  http.StatusOK,
+	}
+}
+
+func (vc *voteUseCase) ValidateValue(value int8) int8 {
+	switch {
+	case value >= 1:
+		return 1
+	case value <= 1:
+		return -1
+	}
+	return 0
 }
