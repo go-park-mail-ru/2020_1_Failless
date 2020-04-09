641ca5f960b730bc92e49887fc0427376d7d8c5a
Egor Bedov <bedovegor2203@gmail.com>
2020-04-09 15:12:57 +0300

    Tests for network (2/3)

---
+++ internal/pkg/network/request_test.go
@@ -0,0 +1,46 @@
+package network
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetIdFromRequest(t *testing.T) {
+	req, err := http.NewRequest("GET", "/api", nil)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	rr := httptest.NewRecorder()
+
+	var ps = make(map[string]string)
+
+	// Correct
+	ps["id"] = "15"
+	assert.Equal(t, 15, GetIdFromRequest(rr, req, &ps))
+	// Incorrect
+	ps["id"] = "kek"
+	assert.Equal(t, -1, GetIdFromRequest(rr, req, &ps))
+}
+
+func TestGetPageFromRequest(t *testing.T) {
+	req, err := http.NewRequest("GET", "/api", nil)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	rr := httptest.NewRecorder()
+
+	var ps = make(map[string]string)
+
+	// Correct
+	ps["page"] = "15"
+	assert.Equal(t, 15, GetPageFromRequest(rr, req, &ps))
+	// Incorrect
+	ps["page"] = "kek"
+	assert.Equal(t, 1, GetPageFromRequest(rr, req, &ps))
+}
