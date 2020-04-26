9c5bbbbf6dbd236e5f54d991854c17073de419c3
Egor Bedov <bedovegor2203@gmail.com>
2020-04-26 20:42:32 +0300

    Switch to development config

--- configs/server/settings.go
+++ configs/server/settings.go
@@ -212,7 +212,7 @@ func GetConfig() *settings.ServerSettings {
 			// it's correct length of CSRF token for Base64 (in bytes)
 			CSRFTokenLen: 20,
 			CSRFTokenTTL: 1, // one hour
-			EnableCSRF:   true,
+			EnableCSRF:   false,
 		}
 		settings.UseCaseConf = settings.GlobalConfig{
 			PageLimit: 10,
