5de03c47fb0ecbbfb4b332f8f5bffd7cacef5888
Egor Bedov <bedovegor2203@gmail.com>
2020-05-18 21:01:04 +0300

    Switch to development config

--- internal/pkg/db/db.go
+++ internal/pkg/db/db.go
@@ -10,7 +10,7 @@ import (
 
 var (
 	// TODO: add it as environment variable
-	host     = "eventumdb"
+	host     = "localhost"
 	port     = uint16(5432)
 	user     = os.Getenv("POSTGRES_USER")
 	password = os.Getenv("POSTGRES_PASSWORD")
