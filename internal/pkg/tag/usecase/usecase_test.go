2ef801c4217c39b8bf579c29b6e09fef80884c69
Egor Bedov <bedovegor2203@gmail.com>
2020-05-19 20:57:12 +0300

    Tag UseCase tests

--- internal/pkg/tag/usecase/usecase_test.go
+++ internal/pkg/tag/usecase/usecase_test.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"errors"
 	"failless/internal/pkg/models"
 	"failless/internal/pkg/tag"
 	"failless/internal/pkg/tag/mocks"
@@ -12,21 +13,37 @@ import (
 
 func getTestUseCase(mockRep *mocks.MockRepository) tag.UseCase {
 	return &tagUseCase{
-		rep: mockRep,
+		Rep: mockRep,
 	}
 }
 
-func TestTagUseCase_InitEventsByTime(t *testing.T) {
+func TestTagUseCase_InitEventsByTime_Correct(t *testing.T) {
 	// Create mock
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 	mockRep := mocks.NewMockRepository(mockCtrl)
-	vc := getTestUseCase(mockRep)
+	tc := getTestUseCase(mockRep)
 
-	mockRep.EXPECT().GetAllTags()
+	mockRep.EXPECT().GetAllTags().Return(nil, nil)
 
-	status, err := vc.InitEventsByTime(new([]models.Tag))
+	status, err := tc.InitEventsByTime(new(models.TagList))
 
 	assert.Equal(t, http.StatusOK, status)
 	assert.Equal(t, nil, err)
+}
+
+func TestTagUseCase_InitEventsByTime_Incorrect(t *testing.T) {
+	// Create mock
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockRep := mocks.NewMockRepository(mockCtrl)
+	tc := getTestUseCase(mockRep)
+
+	correctError := errors.New("internal tag repo error")
+	mockRep.EXPECT().GetAllTags().Return(nil, correctError)
+
+	status, err := tc.InitEventsByTime(new(models.TagList))
+
+	assert.Equal(t, http.StatusInternalServerError, status)
+	assert.Equal(t, correctError, err)
 }
