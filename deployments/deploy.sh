f60c668d276405cab1114ae647f8aededb984ca9
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-05-21 13:35:05 +0300

    updated user

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -4,11 +4,12 @@ set -e
 echo "scp -r ${TRAVIS_BUILD_DIR} a.prokopenko@163.172.133.90:/home/a.prokopenko/eventum/deploy"
 scp -i ~/.ssh/deploy_rsa -r $TRAVIS_BUILD_DIR a.prokopenko@163.172.133.90:/home/a.prokopenko/eventum/deploy
 echo "ssh a.prokopenko@163.172.133.90 -v exit"
-ssh a.prokopenko@163.172.133.90
-echo "cd /home/a.prokopenko/eventum/deploy/2020_1_Failless/deployments"
-cd /home/a.prokopenko/eventum/deploy/2020_1_Failless/deployments
-echo "docker-compose build --env-file /home/a.prokopenko/eventum/back/deployments/.env"
-docker-compose build --env-file /home/a.prokopenko/eventum/back/deployments/.env
-echo "docker-compose up -d"
-docker-compose up -d
-exit
+ssh a.prokopenko@163.172.133.90 'cd /home/a.prokopenko/eventum/deploy/2020_1_Failless/deployments;
+  cp /home/a.prokopenko/eventum/back/deployments/.env . ;
+  sudo docker-compose build ;
+  sudo docker-compose up ; '
+
+#echo "docker-compose up -d"
+#docker-compose up -d
+#echo "cd /home/a.prokopenko/eventum/deploy/2020_1_Failless/deployments"
+#cd /home/a.prokopenko/eventum/deploy/2020_1_Failless/deployments
