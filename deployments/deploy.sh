66c8a422ab4a318621a2d178e7f488eed5c9b073
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-05-21 11:48:13 +0300

    depoy deploy deploy deploy

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+echo "set -e"
 set -e
+echo "scp -r ${TRAVIS_BUILD_DIR} a.prokopenko@163.172.133.90:/home/a.prokopenko/eventum/deploy"
 scp -r $TRAVIS_BUILD_DIR a.prokopenko@163.172.133.90:/home/a.prokopenko/eventum/deploy
+echo "ssh a.prokopenko@163.172.133.90 -v exit"
 ssh a.prokopenko@163.172.133.90 -v exit
+echo "cd /home/a.prokopenko/eventum/deploy/2020_1_Failless/deployments"
 cd /home/a.prokopenko/eventum/deploy/2020_1_Failless/deployments
+echo "docker-compose build --env-file /home/a.prokopenko/eventum/back/deployments/.env"
 docker-compose build --env-file /home/a.prokopenko/eventum/back/deployments/.env
+echo "docker-compose up -d"
 docker-compose up -d
