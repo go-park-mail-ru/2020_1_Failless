5b4b98263c94f292748b01e6bb3f15d9d0707b81
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-05-21 12:20:48 +0300

    test 4

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -2,7 +2,7 @@
 echo "set -e"
 set -e
 echo "scp -r ${TRAVIS_BUILD_DIR} a.prokopenko@163.172.133.90:/home/a.prokopenko/eventum/deploy"
-scp -r $TRAVIS_BUILD_DIR a.prokopenko@163.172.133.90:/home/a.prokopenko/eventum/deploy
+scp -i ~/.ssh/deploy_rsa -r $TRAVIS_BUILD_DIR a.prokopenko@163.172.133.90:/home/a.prokopenko/eventum/deploy
 echo "ssh a.prokopenko@163.172.133.90 -v exit"
 ssh a.prokopenko@163.172.133.90 -v exit
 echo "cd /home/a.prokopenko/eventum/deploy/2020_1_Failless/deployments"
